0f8a41b1e02c7d39fd37ee59d7622461b6f2af41
Victor Campos <mailforvic@gmail.com>
2023-06-05 16:50:10 -0700

    adding a rename project powershell script

---
+++ rename-project.ps1
@@ -0,0 +1,11 @@
+﻿$NamespacePrefix = Read-Host -Prompt 'Solution prefix to replace Repetiva'
+Write-Host "Renaming Repetiva to $NamespacePrefix"
+
+gci -r -include "*.sln","*.csproj","*.cs" |
+ foreach-object { $a = $_.fullname; ( get-content $a ) |
+ foreach-object { $_ -replace 'Repetiva',$NamespacePrefix }  | 
+set-content $a }
+
+Get-ChildItem -Filter *.sln | Rename-Item -NewName { $_.Name -replace 'Repetiva', $NamespacePrefix }
+Get-ChildItem -Path .\ -Directory | Rename-Item -NewName { $_.Name -replace 'Repetiva',$NamespacePrefix }
+Get-ChildItem -Recurse -Filter *.csproj | Rename-Item -NewName { $_.Name -replace 'Repetiva', $NamespacePrefix }
